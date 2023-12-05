201c7cc5b78ad17788b55e5f3fd489808cfe1570
Mike Treit <mtreit@ntdev.microsoft.com>
2023-12-05 10:18:18 -0800

    Fix typo.

--- 2023/csharp/newday.ps1
+++ 2023/csharp/newday.ps1
@@ -3,6 +3,6 @@ param ($DayNumber)
 robocopy /e Template "Day$DayNumber"
 Set-Location "Day$DayNumber"
 fastmod "___" "$DayNumber" --accept-all
-(Invoke-WebRequest -Uri https://adventofcode.com/2023/day/$DayNumber/input -Headers @{ "Cookie" = "session=$envAocSessionCookie" }).Content | Set-Content input.txt -NoNewline
+(Invoke-WebRequest -Uri https://adventofcode.com/2023/day/$DayNumber/input -Headers @{ "Cookie" = "session=$env:AocSessionCookie" }).Content | Set-Content input.txt -NoNewline
 
 start Benchmark.csproj
