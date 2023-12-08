47dd0146e44c427c9f8050cb6ffb785d1eb40626
Mike Treit <mtreit@ntdev.microsoft.com>
2023-12-07 20:00:53 -0800

    Day 7

---
+++ 2023/csharp/Day7/Benchmark.cs
@@ -0,0 +1,90 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+using System;
+
+namespace Test
+{
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Diagnosers;
+    using System.Text.RegularExpressions;
+    using System.IO;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        string[] _input;
+
+        public Benchmark()
+        {
+        }
+
+        public Benchmark(string inputFile)
+        {
+            _input = File.ReadAllLines(inputFile);
+        }
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            _input = File.ReadAllLines("input.txt");
+        }
+
+        [Benchmark]
+        public string Day7Part1()
+        {
+            var hands = new List<(Hand, int)>();
+
+            foreach (var line in _input)
+            {
+                var tokens = line.Split(" ");
+                var hand = new Hand(tokens[0]);
+                var bid = int.Parse(tokens[1]);
+                hands.Add((hand, bid));
+            }
+
+            var rank = 0;
+            var totalWinnings = 0;
+
+            foreach (var hand in hands.OrderBy(x => x.Item1))
+            {
+                rank++;
+                Console.WriteLine($"{hand.Item1.Cards} -> {hand.Item1.Strength}");
+                totalWinnings += rank * hand.Item2;
+            }
+
+            var answer = totalWinnings;
+            return answer.ToString();
+        }
+
+        [Benchmark]
+        public string Day7Part2()
+        {
+            var hands = new List<(WildcardHand, long)>();
+
+            foreach (var line in _input)
+            {
+                var tokens = line.Split(" ");
+                var hand = new WildcardHand(tokens[0]);
+                var bid = long.Parse(tokens[1]);
+
+                hands.Add((hand, bid));
+            }
+
+            var rank = 0L;
+            var totalWinnings = 0L;
+
+            foreach (var hand in hands.OrderBy(x => x.Item1))
+            {
+                rank++;
+                Console.WriteLine($"({rank}) {hand.Item1.Cards} ({hand.Item1.EffectiveCards}) -> {hand.Item1.Strength} -> {totalWinnings} += {rank} * {hand.Item2} == {totalWinnings + rank * hand.Item2}");
+                totalWinnings += rank * hand.Item2;
+            }
+
+            var answer = totalWinnings;
+            return answer.ToString();
+        }
+   }
+}
---
+++ 2023/csharp/Day7/Hand.cs
@@ -0,0 +1,204 @@
+﻿using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    enum HandStrength : long
+    {
+        Unknown = -1,
+        HighCard,
+        OnePair,
+        TwoPair,
+        ThreeOfAKind,
+        Straight,
+        FullHouse,
+        FourOfAKind,
+        FiveOfAKind
+    }
+
+    internal class Hand : IComparable<Hand>
+    {
+        static readonly Dictionary<char, int> CardValues
+            = new Dictionary<char, int>
+            {
+                { 'A', 13 },
+                { 'K', 12 },
+                { 'Q', 11 },
+                { 'J', 10 },
+                { 'T', 9 },
+                { '9', 8 },
+                { '8', 7 },
+                { '7', 6 },
+                { '6', 5 },
+                { '5', 4 },
+                { '4', 3 },
+                { '3', 2 },
+                { '2', 1 },
+            };
+
+        public string Cards { get; private set; }
+
+        public HandStrength Strength { get; private set; }
+
+        public Hand(string handDescription)
+        {
+            Cards = handDescription;
+            Strength = GetStrength(Cards);
+        }
+
+        HandStrength GetStrength(string cards)
+        {
+            var counts = cards
+                .OrderBy(x => x)
+                .GroupBy(x => x)
+                .OrderByDescending(x => x.Count())
+                .Select(x => x.Count());
+
+            var n = counts.Count();
+            var strength = (n, counts.First()) switch
+            {
+                (5, _) => HandStrength.HighCard,
+                (4, _) => HandStrength.OnePair,
+                (3, 2) => HandStrength.TwoPair,
+                (3, 3) => HandStrength.ThreeOfAKind,
+                (2, 3) => HandStrength.FullHouse,
+                (2, 4) => HandStrength.FourOfAKind,
+                (1, _) => HandStrength.FiveOfAKind,
+                _ => HandStrength.Unknown
+            };
+
+            return strength;
+        }
+
+        public int CompareTo(Hand other)
+        {
+            var result = Strength.CompareTo(other.Strength);
+            if (result != 0)
+            {
+                return result;
+            }
+
+            for (int i = 0; i < Cards.Length; i++)
+            {
+                result = CardValues[Cards[i]].CompareTo(CardValues[other.Cards[i]]);
+                if (result != 0)
+                {
+                    return result;
+                }
+            }
+
+            return 0;
+        }
+    }
+
+    internal class WildcardHand : IComparable<WildcardHand>
+    {
+        static readonly Dictionary<char, int> CardValues
+            = new Dictionary<char, int>
+            {
+                { 'A', 13 },
+                { 'K', 12 },
+                { 'Q', 11 },
+                { 'J', 0 }, // Joker, worth the least
+                { 'T', 9 },
+                { '9', 8 },
+                { '8', 7 },
+                { '7', 6 },
+                { '6', 5 },
+                { '5', 4 },
+                { '4', 3 },
+                { '3', 2 },
+                { '2', 1 },
+            };
+
+        public string Cards { get; private set; }
+
+        public string EffectiveCards { get; private set; }
+
+        public HandStrength Strength { get; private set; }
+
+        public WildcardHand(string handDescription)
+        {
+            Cards = handDescription;
+            EffectiveCards = GetEffectiveCards(Cards);
+            Strength = GetStrength(EffectiveCards);
+        }
+
+        HandStrength GetStrength(string cards)
+        {
+            var counts = cards
+                .OrderBy(x => x)
+                .GroupBy(x => x)
+                .OrderByDescending(x => x.Count())
+                .Select(x => x.Count());
+
+            var n = counts.Count();
+            var strength = (n, counts.First()) switch
+            {
+                (5, _) => HandStrength.HighCard,
+                (4, _) => HandStrength.OnePair,
+                (3, 2) => HandStrength.TwoPair,
+                (3, 3) => HandStrength.ThreeOfAKind,
+                (2, 3) => HandStrength.FullHouse,
+                (2, 4) => HandStrength.FourOfAKind,
+                (1, _) => HandStrength.FiveOfAKind,
+                _ => HandStrength.Unknown
+            };
+
+            return strength;
+        }
+
+        public int CompareTo(WildcardHand other)
+        {
+            var result = Strength.CompareTo(other.Strength);
+            if (result != 0)
+            {
+                return result;
+            }
+
+            for (int i = 0; i < Cards.Length; i++)
+            {
+                result = CardValues[Cards[i]].CompareTo(CardValues[other.Cards[i]]);
+                if (result != 0)
+                {
+                    return result;
+                }
+            }
+
+            return 0;
+        }
+
+        private string GetEffectiveCards(string cards)
+        {
+            if (cards.IndexOf('J') == -1)
+            {
+                return cards;
+            }
+
+            cards = new string(cards.OrderBy(x => x).ToArray());
+            var highestValue = cards.ToCharArray().OrderByDescending(x => CardValues[x]).First();
+            var newValue = highestValue;
+            var countOfHighest = cards.Count(x => x == highestValue);
+
+            foreach (var c in cards.Where(x => x != highestValue && x != 'J'))
+            {
+                var tempCount = cards.Count(x => x == c);
+
+                if (tempCount > countOfHighest)
+                {
+                    countOfHighest = tempCount;
+                    newValue = c;
+                }
+            }
+
+            cards = cards.Replace('J', newValue);
+            return cards;
+        }
+    }
+}
