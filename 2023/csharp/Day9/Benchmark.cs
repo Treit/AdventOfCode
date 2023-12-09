4f24cc187c088cc35ce895f405e28ab88d1dca67
Mike Treit <mtreit@ntdev.microsoft.com>
2023-12-09 14:42:09 -0800

    Day 9 with way too many ToArray calls.

---
+++ 2023/csharp/Day9/Benchmark.cs
@@ -0,0 +1,94 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+using System;
+
+namespace Test
+{
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Diagnosers;
+    using System.Text.RegularExpressions;
+    using System.IO;
+    using System;
+    using System.Linq;
+
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        string[] _input;
+
+        public Benchmark()
+        {
+        }
+
+        public Benchmark(string inputFile)
+        {
+            _input = File.ReadAllLines(inputFile);
+        }
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            _input = File.ReadAllLines("input.txt");
+        }
+
+        [Benchmark]
+        public string Day9Part1()
+        {
+            var answer = 0L;
+
+            foreach (var line in _input)
+            {
+                answer += ProcessLine(line, false);
+            }
+
+            return answer.ToString();
+        }
+
+        [Benchmark]
+        public string Day9Part2()
+        {
+            var answer = 0L;
+
+            foreach (var line in _input)
+            {
+                answer += ProcessLine(line, true);
+            }
+
+            return answer.ToString();
+        }
+
+        long ProcessLine(string line, bool reverse)
+        {
+            var nums = line.Split(" ").Select(x => long.Parse(x)).ToArray();
+            var sequence = reverse ? GenerateNextSequence(nums.Reverse().ToArray()) : GenerateNextSequence(nums.ToArray());
+            var answer = sequence[sequence.Length - 1];
+            sequence = reverse ? sequence.Reverse().ToArray() : sequence.ToArray();
+            Console.WriteLine(string.Join(',', sequence));
+            return answer;
+        }
+
+        long[] GenerateNextSequence(long[] input)
+        {
+            if (input.All(x => x == 0))
+            {
+                return input;
+            }
+
+            long[] result = new long[input.Length + 1];
+            Array.Copy(input, result, input.Length);
+
+            long[] tempResult = new long[input.Length - 1];
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                var x = input[i];
+                var y = input[i + 1];
+                tempResult[i] = y - x;
+            }
+
+            var next = GenerateNextSequence(tempResult);
+            result[result.Length - 1] = input[input.Length - 1] + next[next.Length - 1];
+
+            return result;
+        }
+    }
+}
---
+++ 2023/csharp/Day9/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Running;
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+#if RELEASE
+            BenchmarkRunner.Run<Benchmark>();
+#else
+            var inputFile = args.Any(arg => arg.Contains("test")) ? "testinput.txt" : "input.txt";
+
+            var b = new Benchmark(inputFile);
+
+            var runDay1 = args switch
+            {
+                [] => true,
+                ["test"] => true,
+                ["1", ..] => true,
+                [_, "1", ..] => true,
+                _ => false
+            };
+
+            var runDay2 = args switch
+            {
+                [] => true,
+                ["test"] => true,
+                ["2", ..] => true,
+                [_, "2", ..] => true,
+                _ => false
+            };
+
+            if (runDay1)
+            {
+                Console.WriteLine($"Day 9, part 1 answer: {b.Day9Part1()}");
+            }
+
+            if (runDay2)
+            {
+                Console.WriteLine($"Day 9, part 2 answer: {b.Day9Part2()}");
+            }
+#endif
+        }
+    }
+}
