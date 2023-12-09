b2605aeefe913f4415d525c998e0556f8b1f6970
Mike Treit <mtreit@ntdev.microsoft.com>
2023-12-08 17:47:08 -0800

    Cleanup.

--- 2023/csharp/Day2/Program.cs
+++ 2023/csharp/Day2/Program.cs
@@ -1,80 +1,23 @@
-﻿using System.Diagnostics;
-using System.Text.RegularExpressions;
-
-var inputfile = args.Any(arg => arg.Contains("test")) ? "testinput.txt" : "input.txt";
-var input = File.ReadAllLines(inputfile);
-
-if (args.Any(arg => arg.Contains("2")))
+﻿namespace Test
 {
-    Part2(input);
-    return;
-}
+    using BenchmarkDotNet.Running;
+    using System;
 
-Part1(input);
-
-void Part1(string[] input)
-{
-    var maxRed = 12;
-    var maxGreen = 13;
-    var maxBlue = 14;
-    var answer = 0;
-
-    foreach (var line in input)
+    internal class Program
     {
-        var normalized = line.Replace(" ", "");
-        var tokens = normalized.Split(":");
-        var gameId = int.Parse(tokens[0].Replace("Game", ""));
-        var results = tokens[1].Replace(";", ",");
-        tokens = results.Split(",");
-        var valid = true;
-
-        foreach (var token in tokens)
+        static void Main(string[] args)
         {
-            var match = Regex.Match(token, @"(\d+)(\w+)");
-            var num = int.Parse(match.Result("$1"));
-            var color = match.Result("$2");
-            if (color == "green" && num > maxGreen) valid = false;
-            if (color == "red" && num > maxRed) valid = false;
-            if (color == "blue" && num > maxBlue) valid = false;
-        }
-
-        if (valid)
-        {
-            answer += gameId;
+#if RELEASE
+            BenchmarkRunner.Run<Benchmark>();
+#else
+            var b = new Benchmark();
+            b.GlobalSetup();
+            Console.WriteLine($"Day 1, part 1 answer: {b.Day2Part1()}");
+            Console.WriteLine($"Day 2, part 1 optimized answer: {b.Day2Part1Optimized()}");
+            Console.WriteLine($"Day 2, part 2 answer: {b.Day2Part2()}");
+            Console.WriteLine($"Day 2, part 2 optimized answer: {b.Day2Part2Optimized()}");
+            Console.WriteLine($"Day 2, part 2 viceroypenguin: {b.Day2ViceroyPenguin()}");
+#endif
         }
     }
-
-    Console.WriteLine(answer);
-}
-
-void Part2(string[] input)
-{
-    var answer = 0;
-
-    foreach (var line in input)
-    {
-        var normalized = line.Replace(" ", "");
-        var tokens = normalized.Split(":");
-        var gameId = int.Parse(tokens[0].Replace("Game", ""));
-        var results = tokens[1].Replace(";", ",");
-        tokens = results.Split(",");
-        var maxGreenSeen = 0;
-        var maxRedSeen = 0;
-        var maxBlueSeen = 0;
-
-        foreach (var token in tokens)
-        {
-            var match = Regex.Match(token, @"(\d+)(\w+)");
-            var num = int.Parse(match.Result("$1"));
-            var color = match.Result("$2");
-            if (color == "green" && num > maxGreenSeen) maxGreenSeen = num;
-            if (color == "red" && num > maxRedSeen) maxRedSeen = num;
-            if (color == "blue" && num > maxBlueSeen) maxBlueSeen = num;
-        }
-
-        var power = maxGreenSeen * maxRedSeen * maxBlueSeen;
-        answer += power;
-    }
-
-    Console.WriteLine(answer);
 }
