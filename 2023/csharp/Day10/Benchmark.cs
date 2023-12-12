d5a13492490353a1b428674a26fb68a71790cb4f
Mike Treit <mtreit@ntdev.microsoft.com>
2023-12-11 18:19:52 -0800

    Day 10 Part 2 - pre-cleanup

--- 2023/csharp/Day10/Benchmark.cs
+++ 2023/csharp/Day10/Benchmark.cs
@@ -9,6 +9,8 @@ namespace Test
     using System.Text.RegularExpressions;
     using System.IO;
     using System;
+    using System.Text;
+    using System.Linq;
 
     [MemoryDiagnoser]
     public class Benchmark
@@ -58,6 +60,11 @@ namespace Test
             }
 
             var answer = Pipes.WalkPartOne(startPos, _input);
+            var answer2 = Pipes.WalkBackToStart(startPos, _input);
+
+            Console.WriteLine(answer);
+            Console.WriteLine(answer2);
+            Console.ReadKey();
 
             return answer.ToString();
         }
@@ -65,7 +72,6 @@ namespace Test
         [Benchmark]
         public string Day10Part2()
         {
-            using var sw = new StreamWriter(@"input2.txt");
             var startPos = (0, 0);
             var row = -1;
             var col = -1;
@@ -83,23 +89,65 @@ namespace Test
                         Console.BackgroundColor = ConsoleColor.Green;
                     }
 
-                    sw.Write(Pipes.SymbolMap[c]);
-                    Console.Write(Pipes.SymbolMap[c]);
+                    //Console.Write(Pipes.SymbolMap[c]);
                     Console.ResetColor();
                 }
 
-                sw.WriteLine();
-                Console.WriteLine();
+                //Console.WriteLine();
             }
 
             //var answer = Pipes.Walk(startPos, _input);
             var positions = Pipes.GetAllPipeLocations(startPos, _input);
-            foreach (var pos in positions)
+            var grid = Pipes.GetGrid(_input);
+            var scaledGrid = Pipes.RescaleGrid(grid, 2);
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < grid.Length; i++)
             {
-                Console.WriteLine(pos);
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (positions.Contains((i, j)))
+                    {
+                        scaledGrid[i * 2][j * 2] = (Pipes.SymbolMap[grid[i][j]]);
+                    }
+                }
             }
 
-            var answer = 0;
+            Pipes.WalkAndFill((startPos.Item1 * 2, startPos.Item2 * 2), scaledGrid);
+            Pipes.FloodFill(scaledGrid, '.', '~');
+
+            for (int i = 0; i < scaledGrid.Length; i++)
+            {
+                for (int j = 0; j < scaledGrid[i].Length; j++)
+                {
+                    sb.Append(scaledGrid[i][j]);
+                }
+
+                sb.Append("\n");
+            }
+
+            var str = sb.ToString();
+            Console.WriteLine(str);
+
+            var scaledGrid2 = Pipes.RescaleGrid(scaledGrid, -2);
+
+            sb.Clear();
+
+            for (int i = 0; i < scaledGrid2.Length; i++)
+            {
+                for (int j = 0; j < scaledGrid2[i].Length; j++)
+                {
+                    sb.Append(scaledGrid[i * 2][j * 2]);
+                }
+
+                sb.Append("\n");
+            }
+
+            str = sb.ToString();
+            Console.WriteLine(str);
+
+            var answer = str.Count(x => x == '.');
             return answer.ToString();
         }
    }
--- 2023/csharp/Day10/Pipes.cs
+++ 2023/csharp/Day10/Pipes.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Diagnostics.Tracing.Parsers.Clr;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -32,10 +33,6 @@ namespace Test
             var row = startPos.Row;
             var col = startPos.Col;
             var startingDirections = PossibleStartingDirections(startPos, input);
-            foreach (var dir in startingDirections)
-            {
-                Console.WriteLine(dir);
-            }
 
             var currentDir = startingDirections.First();
 
@@ -101,12 +98,68 @@ namespace Test
             return moves;
         }
 
+        public static int WalkBackToStart((int Row, int Col) startPos, string[] input)
+        {
+            var rowA = startPos.Row;
+            var colA = startPos.Col;
+            var currentDirA = Direction.Up;
+
+            var currentTileA = SymbolMap[input[rowA][colA]];
+
+            var moves = 0;
+
+            while (true)
+            {
+                if (moves > 1 && currentTileA == 'S')
+                {
+                    break;
+                }
+
+                var (newTileA, newDirA) = DoMove(ref rowA, ref colA, currentTileA, currentDirA, input);
+
+                currentDirA = newDirA;
+                currentTileA = newTileA;
+
+                moves++;
+            }
+
+            return moves;
+        }
+
+        public static int WalkAndFill((int Row, int Col) startPos, char[][] input)
+        {
+            var rowA = startPos.Row;
+            var colA = startPos.Col;
+            var currentDirA = Direction.Up;
+
+            var currentTileA = input[rowA][colA];
+
+            var moves = 0;
+
+            while (true)
+            {
+                if (moves > 1 && currentTileA == 'S')
+                {
+                    break;
+                }
+
+                var (newTileA, newDirA) = DoMovePart2(ref rowA, ref colA, currentTileA, currentDirA, input);
+
+                currentDirA = newDirA;
+                currentTileA = newTileA;
+
+                moves++;
+            }
+
+            return moves;
+        }
+
         public static Direction[] PossibleStartingDirections((int Row, int Col) startPos, string[] input)
         {
             var result = new List<Direction>();
-            if (SymbolMap[input[startPos.Row - 1][startPos.Col]] == '║'
-                || SymbolMap[input[startPos.Row - 1][startPos.Col]] == '╗'
-                || SymbolMap[input[startPos.Row - 1][startPos.Col]] == '╔')
+            if (SymbolMap[input[Math.Max(0, startPos.Row - 1)][startPos.Col]] == '║'
+                || SymbolMap[input[Math.Max(0, startPos.Row - 1)][startPos.Col]] == '╗'
+                || SymbolMap[input[Math.Max(0, startPos.Row - 1)][startPos.Col]] == '╔')
             {
                 result.Add(Direction.Up);
             }
@@ -133,7 +186,7 @@ namespace Test
             }
 
             return result.ToArray();
-            
+
         }
 
         static (char NewTile, Direction NewDirection) DoMove(
@@ -165,6 +218,160 @@ namespace Test
             };
 
             return (newTile, newDir);
+        }
+
+        static (char NewTile, Direction NewDirection) DoMovePart2(
+            ref int row,
+            ref int col,
+            char currentTile,
+            Direction currentDir,
+            char[][] input)
+        {
+            var (newTile, newDir) = currentDir switch
+            {
+                Direction.Up when currentTile is 'S' => (input[--row][col], Direction.Up),
+                Direction.Up when currentTile is '║' => (input[--row][col], Direction.Up),
+                Direction.Up when currentTile is '╗' => (input[row][--col], Direction.Left),
+                Direction.Up when currentTile is '╔' => (input[row][++col], Direction.Right),
+                Direction.Down when currentTile is 'S' => (input[++row][col], Direction.Down),
+                Direction.Down when currentTile is '║' => (input[++row][col], Direction.Down),
+                Direction.Down when currentTile is '╚' => (input[row][++col], Direction.Right),
+                Direction.Down when currentTile is '╝' => (input[row][--col], Direction.Left),
+                Direction.Left when currentTile is 'S' => (input[row][--col], Direction.Left),
+                Direction.Left when currentTile is '═' => (input[row][--col], Direction.Left),
+                Direction.Left when currentTile is '╚' => (input[--row][col], Direction.Up),
+                Direction.Left when currentTile is '╔' => (input[++row][col], Direction.Down),
+                Direction.Right when currentTile is 'S' => (input[row][++col], Direction.Right),
+                Direction.Right when currentTile is '═' => (input[row][++col], Direction.Right),
+                Direction.Right when currentTile is '╝' => (input[--row][col], Direction.Up),
+                Direction.Right when currentTile is '╗' => (input[++row][col], Direction.Down),
+                _ => (currentTile, currentDir)
+            };
+
+            newTile = (newDir, currentTile) switch
+            {
+                (Direction.Up, '.') => '║',
+                (Direction.Down, '.') => '║',
+                (Direction.Left, '.') => '═',
+                (Direction.Right, '.') => '═',
+                _ => newTile
+            };
+
+            input[row][col] = newTile;
+
+            return (newTile, newDir);
+        }
+
+        public static char[][] GetGrid(string[] input)
+        {
+            var result = new char[input.Length][];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                result[i] = input[i].ToCharArray();
+            }
+
+            return result;
+        }
+
+        public static char[][] RescaleGrid(char[][] originalGrid, int scaleFactor)
+        {
+            var divide = scaleFactor < 0 ? true : false;
+            scaleFactor = Math.Abs(scaleFactor);
+            var newGrid = divide ? new char[originalGrid.Length / scaleFactor][] : new char[originalGrid.Length * scaleFactor][];
+            for (int i = 0; i < newGrid.Length; i++)
+            {
+                newGrid[i] = divide ? new char[originalGrid[0].Length / scaleFactor] : new char[originalGrid[0].Length * scaleFactor];
+                Array.Fill(newGrid[i], '.');
+            }
+
+            return newGrid;
+        }
+
+        public static void FloodFill(char[][] input, char target, char replacement)
+        {
+            var row = 0;
+            var col = 0;
+            var pos = (row, col);
+
+            var q = new Queue<(int, int)>();
+            q.Enqueue(pos);
+
+            while (q.Count > 0)
+            {
+                var loc = q.Dequeue();
+                var tile = input[loc.Item1][loc.Item2];
+                if (tile == target)
+                {
+                    input[loc.Item1][loc.Item2] = replacement;
+
+                    // West
+                    if (loc.Item2 > 0)
+                    {
+                        q.Enqueue((loc.Item1, loc.Item2 - 1));
+                    }
+
+                    // East
+                    if (loc.Item2 < input[0].Length - 1)
+                    {
+                        q.Enqueue((loc.Item1, loc.Item2 + 1));
+                    }
+
+                    // North
+                    if (loc.Item1 > 0)
+                    {
+                        q.Enqueue((loc.Item1 - 1, loc.Item2));
+                    }
+
+                    // South
+                    if (loc.Item1 < input.Length - 1)
+                    {
+                        q.Enqueue((loc.Item1 + 1, loc.Item2));
+                    }
+                }
+            }
+        }
+
+        public static void FillRemainingOutsidePoints(char[][] input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    char current = input[i][j];
+                    if (current != '.')
+                    {
+                        continue;
+                    }
+
+                    var edgeCount = 0;
+                    bool outside = true;
+
+                    for (int k = j; k < input[i].Length; k++)
+                    {
+                        if (input[i][k] == '~' || input[i][k] == '.')
+                        {
+                            if (!outside)
+                            {
+                                edgeCount++;
+                            }
+
+                            outside = true;
+                        }
+                        else
+                        {
+                            if (outside)
+                            {
+                                edgeCount++;
+                            }
+
+                            outside = false;
+                        }
+                    }
+
+                    Console.WriteLine(edgeCount % 2);
+                }
+            }
         }
     }
 }
